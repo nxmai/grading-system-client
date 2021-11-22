6fea65159a0f1cb1c4be3fee0930df190fab06cc
nganphan <kimngandinhphan@gmail>
2021-11-22 23:14:45 +0700

    auto direct

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if(!token) {
+      router.push('/auth/login');
+    }
+  },[]);
+
   return <Component {...pageProps} />;
 }
 export default MyApp;
